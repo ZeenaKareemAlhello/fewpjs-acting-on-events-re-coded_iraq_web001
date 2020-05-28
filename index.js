15cc284edfacb294b25bc588931108dc789d6b3c
zeena kareem <zena34269@gmail.com>
2020-05-28 12:56:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function moveDodgerRight() {
  let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
  
-  if (left > 0) {
+  if (right > 0) {
     dodger.style.left = `${left + 1}px`;
   }
 }
